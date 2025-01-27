26f4769ed82b785f54d54ad3ebf9c23978df9143
Meghana <meghanamanchal@gmail.com>
2025-01-27 10:38:10 +0530

    feat: enhance video download functionality with error handling and download link

--- backend.py
+++ backend.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI,Form
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from fastapi import HTTPException
 
 app = FastAPI()
 
@@ -18,21 +20,21 @@ cur_dir = os.getcwd()
 
 @app.post("/download")
 def download_video(link:str = Form(...)):
+    video_filename = f"Video-{link[-11:]}.mp4"
+    video_file_path = os.path.join(cur_dir,video_filename)
+
     youtube_dl_options = {
         "format": "best",
-        "output":os.path.join(cur_dir,f"Video-{link[-11:]}.mp4")
+        "output": video_file_path
     }
-    with yt_dlp.YoutubeDL(youtube_dl_options) as ydl:
-        ydl.download([link])
-    return {"status":"Download started"}
+    try:
+        with yt_dlp.YoutubeDL(youtube_dl_options) as ydl:
+            ydl.download([link])
+        if not os.path.exists(video_file_path):
+            raise HTTPException(status_code=404,detail="Video not found")
+    
+        return FileResponse(video_file_path,media_type="video/mp4",filename=video_filename)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to download video: {str(e)}")
 
-# download_video('https://www.youtube.com/watch?v=EixIyh1gshM')
-# @app.post("/fetchQualityofVideo")
-# def download_video(link:str = Form(...)):
-#     youtube_dl_options = {
-#         "format": "best",
-#         "output":os.path.join(cur_dir,f"Video-{link[-11:]}.mp4")
-#     }
-#     with yt_dlp.YoutubeDL(youtube_dl_options) as ydl:
-#         ydl.__sizeof__([link])
-#     return {"status":"Download started"}
+
